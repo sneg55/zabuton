95526ca448c90e74cf5f148cfb940b1054ec74e0
sneg55 <nikita@sawinyh.ru>
2026-09-11 18:24:49 -0400

    feat: serve the frontend from convex.site via static hosting

---
+++ convex/convex.config.ts
@@ -0,0 +1,7 @@
+import staticHosting from "@convex-dev/static-hosting/convex.config";
+import { defineApp } from "convex/server";
+
+const app = defineApp();
+app.use(staticHosting);
+
+export default app;
--- convex/http.ts
+++ convex/http.ts
@@ -1,8 +1,11 @@
+import { registerStaticRoutes } from "@convex-dev/static-hosting";
 import { httpRouter } from "convex/server";
+import { components } from "./_generated/api";
 import { auth } from "./auth";
 
 const http = httpRouter();
 
 auth.addHttpRoutes(http);
+registerStaticRoutes(http, components.staticHosting);
 
 export default http;
